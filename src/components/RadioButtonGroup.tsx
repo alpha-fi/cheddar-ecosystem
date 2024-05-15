a0d2f3de04ec7a5be5861956b02e435444911efb
Mart-Dan-Rossi <94561321+Mart-Dan-Rossi@users.noreply.github.com>
2024-05-15 16:37:48 -0300

    Change select button for radio kind ones and cost/token icon display

---
+++ src/components/RadioButtonGroup.tsx
@@ -0,0 +1,68 @@
+import { Box, HStack, useRadio, useRadioGroup } from '@chakra-ui/react';
+import { isCurrentBrowserSupported } from '@near-wallet-selector/core';
+
+export const RadioCard = (props: any) => {
+  const { getInputProps, getRadioProps } = useRadio(props);
+
+  const input = getInputProps();
+  const checkbox = getRadioProps();
+
+  return (
+    <Box as="label">
+      <input {...input} />
+      <Box
+        {...checkbox}
+        cursor="pointer"
+        borderWidth="1px"
+        borderRadius="md"
+        boxShadow="md"
+        background={props.optionSelected === props.children ? '#ECC94B' : ''}
+        _checked={{
+          bg: 'teal.600',
+          color: 'white',
+          borderColor: 'teal.600',
+        }}
+        _focus={{
+          boxShadow: 'outline',
+        }}
+        px={5}
+        py={3}
+      >
+        {props.children}
+      </Box>
+    </Box>
+  );
+};
+
+export const RadioButtonBroup = (props: any) => {
+  const { options, optionSelected, setOptionSelected } = props;
+  const { getRootProps, getRadioProps } = useRadioGroup({
+    name: 'framework',
+    defaultValue: 'react',
+  });
+
+  const group = getRootProps();
+
+  return (
+    <HStack {...group}>
+      {options.map((option: any) => {
+        const { name } = option;
+        const radio = getRadioProps({ name });
+        function onChange() {
+          setOptionSelected(name);
+        }
+
+        return (
+          <RadioCard
+            key={name}
+            {...radio}
+            optionSelected={optionSelected}
+            onChange={onChange}
+          >
+            {name}
+          </RadioCard>
+        );
+      })}
+    </HStack>
+  );
+};
