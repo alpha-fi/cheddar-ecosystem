92796d999190e536a898003fad7a4d263249c9fe
Mart-Dan-Rossi <martinrossi1993@gmail.com>
2024-09-05 14:55:53 -0300

    Fix bid with Cheddar and Neko en checkers

--- src/contracts/checkers/checkersCalls.ts
+++ src/contracts/checkers/checkersCalls.ts
@@ -300,7 +300,7 @@ export async function selectOpponent(
 
 export const makeAvailableFt = async (
   wallet: Wallet,
-  deposit: string | bigint,
+  deposit: string,
   tokenId: string,
   accountId: string,
   referrerId = ''
@@ -316,7 +316,7 @@ export const makeAvailableFt = async (
           methodName: 'ft_transfer_call',
           args: {
             receiver_id: checkers,
-            amount: Number(deposit),
+            amount: deposit,
             msg: '',
           },
           deposit: '1',
