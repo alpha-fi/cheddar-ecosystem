b663d63c3baec567e5b61eb99fd6b8eeb2fad629
KcPele <fidekg123@gmail.com>
2024-09-25 17:13:35 +0100

    wallet selection

--- src/components/Navbar/components/ButtonConnectWallet.tsx
+++ src/components/Navbar/components/ButtonConnectWallet.tsx
@@ -17,6 +17,7 @@ import { useGetCheddarBalance } from '@/hooks/cheddar';
 import { useAccount, useConnect, useDisconnect } from 'wagmi';
 import { coinbaseWallet } from 'wagmi/connectors';
 import { useEffect } from 'react';
+
 type Props = {
   handleButtonCLick?: () => void;
   text?: string;
@@ -42,15 +43,26 @@ export function ButtonConnectWallet({ handleButtonCLick, text }: Props) {
       walletSelector.modal.show();
     }
   };
+
   useEffect(() => {
-    if (isSuccess) {
-      handleOnClick();
-    }
-    if (address && walletSelector.accountId) {
-      console.log('disconnect base');
-      disconnectBase();
-    }
-  }, [isSuccess, walletSelector]);
+    const handleLogin = async () => {
+      if (address) {
+        console.log('Base Wallet Connected:', isSuccess);
+        await handleOnClick(); // Attempt to log in to NEAR
+      } else {
+        console.log('NEAR Wallet Connected, Disconnecting Base');
+        disconnectBase(); // Disconnect from Base only if NEAR is logged in
+      }
+    };
+
+    handleLogin();
+  }, [isSuccess, walletSelector, address, disconnectBase]);
+
+  const handleSwitchToBase = () => {
+    // Ensure this is triggered directly by user interaction
+    connect({ connector: coinbaseWallet() });
+  };
+
   return (
     <>
       {walletSelector.selector.isSignedIn() ? (
@@ -89,9 +101,8 @@ export function ButtonConnectWallet({ handleButtonCLick, text }: Props) {
                 {smartTrim(walletSelector.accountId ?? '', 12)}
               </Link>
             </MenuItem>
-            <MenuItem onClick={() => connect({ connector: coinbaseWallet() })}>
-              Swtich to Base
-            </MenuItem>
+            {/* Trigger switch to Base with user interaction */}
+            <MenuItem onClick={handleSwitchToBase}>Switch to Base</MenuItem>
             <MenuItem onClick={handleOnClick}>Log out</MenuItem>
           </MenuList>
         </Menu>
