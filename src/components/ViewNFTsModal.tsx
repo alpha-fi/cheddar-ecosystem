bb2a4a9dbbb024aff725996e570726f3dcf41d55
Mart-Dan-Rossi <martinrossi1993@gmail.com>
2024-12-02 12:31:12 -0300

    Merge fix

--- src/components/ViewNFTsModal.tsx
+++ src/components/ViewNFTsModal.tsx
@@ -4,6 +4,7 @@ import { useWalletSelector } from '@/contexts/WalletSelectorContext';
 import { useContext } from 'react';
 import { getConfig } from '@/configs/config';
 import { GameContext } from '@/contexts/maze/GameContextProvider';
+import { useGlobalContext } from '@/contexts/GlobalContext';
 
 interface Props {
   onClose: () => void;
@@ -12,22 +13,25 @@ interface Props {
 
 export const ModalViewNFTs = ({ isOpen, onClose }: Props) => {
   const { nfts } = useContext(GameContext);
+  const { isLoadingCheddarNFTs } = useGlobalContext();
   const { nftImageBaseUrl } = getConfig().networkData;
 
   return (
     <ModalContainer title="Your NFTs" onClose={onClose} isOpen={isOpen}>
       <Stack spacing={3}>
-        {nfts.map((nft) => {
-          return (
-            <div>
-              <Image
-                src={`${nftImageBaseUrl}${nft.token_id}.png`}
-                alt={`chedar nft ${nft.token_id}`}
-              />
-              <h3> Token {nft.token_id}</h3>
-            </div>
-          );
-        })}
+        {nfts && isLoadingCheddarNFTs
+          ? nfts.map((nft) => {
+              return (
+                <div>
+                  <Image
+                    src={`${nftImageBaseUrl}${nft.token_id}.png`}
+                    alt={`chedar nft ${nft.token_id}`}
+                  />
+                  <h3> Token {nft.token_id}</h3>
+                </div>
+              );
+            })
+          : "You don't have NFTs"}
       </Stack>
     </ModalContainer>
   );
